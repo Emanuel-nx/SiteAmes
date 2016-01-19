b38ce7f3248ad6bde6ce23502ce34bc30bdd1d44
Emanuel <emanuelnogueira@inforgeneses.com.br>
2016-01-19 17:12:17 -0300

    atualização

---
+++ enviarEmail.php
@@ -0,0 +1,22 @@
+<?php
+
+	$para= "moises@amesteresina.netau.net";
+	$assunto= "Contato pelo Site";
+	$nome= $_REQUEST['name'];
+	$fone= $_REQUEST['phone'];
+	$email= $_REQUEST['email'];
+	$msg= $_REQUEST['message'];
+
+			$corpo ="<strong> Mensagem de Contato </strong><br><br>";
+			$corpo .= "<strong> Nome: </strong> $nome";
+			$corpo .= "<br><strong> Telefone: </strong> $fone";	
+			$corpo .= "<br><strong> Email: </strong> $email";	
+			$corpo .= "<br><strong> Mensagem: </strong> $msg";	
+
+			$header= "Content-type: text/html; charset=utf-8\n";
+	  		$header.= "From: $email Reply-to: $email\n";
+	  		
+
+	mail($para, $assunto, $corpo,$header);
+
+?>
